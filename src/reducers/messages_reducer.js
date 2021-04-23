6ef423f739d62efe89933658a2186b367066b86c
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-23 17:17:49 +0200

    created reducer files for keys in initialState, just identity reducer for now

---
+++ src/reducers/messages_reducer.js
@@ -0,0 +1,7 @@
+// all reducers starting with the 'identity function' -- fn that returns the same value that is passed in as an argument
+export default function (state = null, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

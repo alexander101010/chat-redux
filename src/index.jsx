b2fa13edaa02470151a316d2fb6051d4b97e2bb5
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-23 17:18:49 +0200

    middleware config and setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,40 @@
 // external modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { logger } from "redux-logger";
+import reduxPromise from "redux-promise";
 
 // internal modules
-import App from './components/app';
-import '../assets/stylesheets/application.scss';
+import App from "./components/app";
+import "../assets/stylesheets/application.scss";
 
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messages: MessagesReducer,
+  channels: ChannelsReducer,
+  selectedChannel: SelectedChannelReducer,
+  currentUser: CurrentUserReducer,
 });
+
+// Middlewares
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+
+const initialState = {
+  messages: [],
+  channels: ["general", "react", "paris"],
+  selectedChannel: "general",
+  currentUser:
+    prompt("What is your username?") ||
+    `anonymous${Math.floor(10 + Math.random() * 90)}`,
+};
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, { initialState }, middlewares)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

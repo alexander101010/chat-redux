c2661d2189c5464ed8bded2a996770423fa111b9
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-26 23:36:53 +0200

    messages reducer

--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -1,6 +1,18 @@
-// all reducers starting with the 'identity function' -- fn that returns the same value that is passed in as an argument
+import { FETCH_MESSAGES, MESSAGE_POSTED, CHANNEL_SELECTED } from "../actions";
+
 export default function (state = null, action) {
   switch (action.type) {
+    case FETCH_MESSAGES: {
+      return action.payload.messages;
+    }
+    case MESSAGE_POSTED: {
+      const copiedState = state.slice(0);
+      copiedState.push(action.payload);
+      return copiedState;
+    }
+    case CHANNEL_SELECTED: {
+      return [];
+    }
     default:
       return state;
   }

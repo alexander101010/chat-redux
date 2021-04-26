e0aa396f93f48b1c4430d83195b7fa65f6a9f039
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-26 23:21:45 +0200

    cleaned up reducers and store

--- src/index.jsx
+++ src/index.jsx
@@ -11,29 +11,35 @@ import App from "./components/app";
 import "../assets/stylesheets/application.scss";
 
 // State and reducers
-const reducers = combineReducers({
-  messages: MessagesReducer,
-  channels: ChannelsReducer,
-  selectedChannel: SelectedChannelReducer,
-  currentUser: CurrentUserReducer,
-});
-
-// Middlewares
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+import messagesReducer from "./reducers/messages_reducer";
+import selectedChannelReducer from "./reducers/selected_channel_reducer";
+const identityReducer = (state = null) => state;
 
 const initialState = {
   messages: [],
-  channels: ["general", "react", "paris"],
+  channels: ["general", "react", "paris", "amsterdam"],
   selectedChannel: "general",
   currentUser:
     prompt("What is your username?") ||
     `anonymous${Math.floor(10 + Math.random() * 90)}`,
 };
 
+const reducers = combineReducers({
+  messages: messagesReducer,
+  channels: identityReducer,
+  selectedChannel: selectedChannelReducer,
+  currentUser: identityReducer,
+});
+
+// Middlewares
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+
+const store = createStore(reducers, initialState, middlewares);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, { initialState }, middlewares)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
